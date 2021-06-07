2191f6b965222391924a3d4dac63fb2127f7b991
Jeremy Brubaker <jbru362@gmail.com>
2021-06-07 14:31:09 -0400

    Apply dmenu-listfullwidth-5.0.diff

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
