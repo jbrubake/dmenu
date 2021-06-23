907149972f2a7624b75fc5d81ae54f78174f168f
Jeremy Brubaker <jbru362@gmail.com>
2021-06-23 19:20:15 +0000

    Add support for color emojis

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10", "Noto Color Emoji: size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
